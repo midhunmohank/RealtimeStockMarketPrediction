f97a1feda9b980f5b1327b72c3a63602a286efbe
Midhun Mohan <k.midhun@outlook.com>
2023-03-23 03:55:51 -0400

    web app

---
+++ frontend/Home.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+import plotly.express as px
+
+def app():
+    # Create a selectbox for the user to choose a stock
+    symbol = st.sidebar.selectbox('Select a stock', ('AAPL', 'MSFT', 'GOOGL'))
+
+    # Use yfinance to retrieve stock data
+    stock_data = yf.Ticker(symbol).history(period="max")
+
+    # Create a line chart using Plotly Express
+    fig = px.line(stock_data, x=stock_data.index, y='Close')
+
+    # Display the chart using Streamlit
+    st.plotly_chart(fig)
+
+
+if __name__ == '__main__':
+    app()
