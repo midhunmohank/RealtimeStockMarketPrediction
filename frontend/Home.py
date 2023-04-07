6dd2f099905ca608213eea9211d03330e717e03d
Midhun Mohan <k.midhun@outlook.com>
2023-04-06 20:39:50 -0400

    updated databases - graph relational

--- frontend/Home.py
+++ frontend/Home.py
@@ -20,3 +20,15 @@ def app():
 
 if __name__ == '__main__':
     app()
+# import requests
+# import json
+
+# url = "https://prod-10.eastus.logic.azure.com/workflows/f737c591a08d423f95455787032d3d60/triggers/manual/paths/invoke/recievesymbol?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=po8AR58-CTucTwOKp3tR379yNJASY2jYlGj56HYrp_w"
+
+# data = {"symbol": "AAPL"}
+# headers = {"Content-Type": "application/json"}
+
+# response = requests.post(url, data=json.dumps(data), headers=headers)
+
+# print(response.status_code)
+# print(response.text)
